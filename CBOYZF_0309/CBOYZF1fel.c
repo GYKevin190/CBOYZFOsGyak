8defa60973e8ab7130e39a3b8e7abf11eff34ddf
GYKevin190 <99425596+GYKevin190@users.noreply.github.com>
2022-03-11 19:24:18 +0100

    5.heti gyakorlat

---
+++ CBOYZF_0309/CBOYZF1fel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    if(!system("dir"))
+    {
+        for(int i =0; i < 3; i++)
+        {
+            printf("CBOYZF\n");
+        }
+    }
+    system("Nincs ilyen parancs");
+    return 0;
+}
---
+++ CBOYZF_0309/child.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        printf("Gyáni Kevin Zsolt CBOYZF\n");
+    }
+
+    return 0;
+}
